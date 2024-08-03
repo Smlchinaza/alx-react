bcae6ace53cc6896adc7698102796d16dffa3cca
Smlchinaza <samchuks898@gmai.com>
2024-08-03 19:08:39 +0000

    react

---
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/utils/utils.js
@@ -0,0 +1,13 @@
+
+rt const getFullYear = () => {
+  return 2021;
+}
+
+export const getFooterCopy = isIndex => {
+  if (isIndex) return 'Holberton School';
+  else return 'Holberton School main dashboard';
+}
+
+export const getLatestNotification = () => {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
